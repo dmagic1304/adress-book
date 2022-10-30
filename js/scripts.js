c32e51481a9d6a2f3c96bead65c88de324eba57d
Dominik Magic <dominikmagic5@gmail.com>
2022-10-29 17:17:23 -0700

    finished the project, it will create addresBook, add contacts and assign them unique ids, find contacts and delete contacts

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,34 @@
-const AddressBook = function() {
+function AddressBook() {
   this.contacts = {};
   this.id = 0;
 }
 
-const Contact = function(firstName, lastName, number) {
+AddressBook.prototype.addContact = function(contact) {
+  contact.id = this.assignId();
+  this.contacts[contact.id] = contact;
+};
+
+AddressBook.prototype.assignId = function() {
+  this.id += 1;
+  return this.id;
+};
+
+AddressBook.prototype.findContact = function(id) {
+  if (this.contacts[id] !== undefined) {
+    return this.contacts[id];
+  }
+  return false;
+};
+
+AddressBook.prototype.deleteContact = function(id) {
+  if (this.contacts[id] === undefined) {
+    return false;
+  }
+  delete this.contacts[id];
+  return true;
+};
+
+function Contact(firstName, lastName, number) {
   this.name = firstName;
   this.lastName = lastName;
   this.number = number;
