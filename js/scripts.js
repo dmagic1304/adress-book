29c6dd0c7fcb0c778c506feac5b4e89623f96605
Dominik Magic <dominikmagic5@gmail.com>
2022-11-01 18:48:48 -0700

    add email and address input fields and add them to the contact details

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
+// Business Logic for AddressBook ---------
 function AddressBook() {
   this.contacts = {};
-  this.id = 0;
+  this.currentId = 0;
 }
 
 AddressBook.prototype.addContact = function(contact) {
@@ -9,8 +10,8 @@ AddressBook.prototype.addContact = function(contact) {
 };
 
 AddressBook.prototype.assignId = function() {
-  this.id += 1;
-  return this.id;
+  this.currentId += 1;
+  return this.currentId;
 };
 
 AddressBook.prototype.findContact = function(id) {
@@ -28,9 +29,73 @@ AddressBook.prototype.deleteContact = function(id) {
   return true;
 };
 
-function Contact(firstName, lastName, number) {
-  this.name = firstName;
+// Business Logic for Contacts ---------
+function Contact(firstName, lastName, phoneNumber, email, address) {
+  this.firstName = firstName;
   this.lastName = lastName;
-  this.number = number;
+  this.phoneNumber = phoneNumber;
+  this.email = email;
+  this.address = address;
 }
 
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+};
+
+// User Interface Logic ---------
+let addressBook = new AddressBook();
+
+function listContacts(addressBookToDisplay) {
+  let contactsDiv = document.querySelector("div#contacts");
+  contactsDiv.innerText =  null;
+  const ul = document.createElement("ul");
+  Object.keys(addressBookToDisplay.contacts).forEach(function(key) {
+    const contact = addressBookToDisplay.findContact(key);
+    const li = document.createElement("li");
+    li.append(contact.fullName());
+    li.setAttribute("id", contact.id);
+    ul.append(li);
+  });
+  contactsDiv.append(ul);
+}
+
+function displayContactDetails(event) {
+  const contact = addressBook.findContact(event.target.id);
+  document.querySelector(".first-name").innerText = contact.firstName;
+  document.querySelector(".last-name").innerText = contact.lastName;
+  document.querySelector(".phone-number").innerText = contact.phoneNumber;
+  document.querySelector(".email").innerText = contact.email;
+  document.querySelector(".address").innerText = contact.address;
+  document.querySelector("button.delete").setAttribute("id", contact.id);
+  document.querySelector("div#contact-details").removeAttribute("class");
+}
+
+function handleDelete(event) {
+  addressBook.deleteContact(event.target.id);
+  document.querySelector("button.delete").removeAttribute("id");
+  document.querySelector("div#contact-details").setAttribute("class", "hidden");
+  listContacts(addressBook);
+}
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  const inputtedFirstName = document.querySelector("input#new-first-name").value;
+  const inputtedLastName = document.querySelector("input#new-last-name").value;
+  const inputtedPhoneNumber = document.querySelector("input#new-phone-number").value;
+  const inputtedEmail = document.querySelector("input#new-email").value;
+  const inputtedAddress = document.querySelector("input#new-address").value;
+  let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber, inputtedEmail, inputtedAddress);
+  addressBook.addContact(newContact);
+  listContacts(addressBook);
+  document.querySelector("input#new-first-name").value = null;
+  document.querySelector("input#new-last-name").value = null;
+  document.querySelector("input#new-phone-number").value = null;
+  document.querySelector("input#new-email").value = null;
+  document.querySelector("input#new-address").value = null;
+}
+
+window.addEventListener("load", function (){
+  document.querySelector("form#new-contact").addEventListener("submit", handleFormSubmission);
+  document.querySelector("div#contacts").addEventListener("click", displayContactDetails);
+  document.querySelector("button.delete").addEventListener("click", handleDelete);
+});
